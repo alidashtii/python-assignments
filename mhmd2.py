09531e5d55b3b9f2b8a5e3280a75f3d37c4d7be2
Ali Dashti <alidashti.r@gmail.com>
2018-05-28 15:56:34 +0430

    tamrin 2

---
+++ mhmd2.py
@@ -0,0 +1,6 @@
+#برنامه‌ای بنویس که یه لیست از اعداد که با کاما از هم جدا شدن را بعنوان ورودی بگیره از ترمینال و بعنوان خروجی یه لیست (list) و یه تاپل (tuple) از همه ی اون اعداد خروجی بده
+def getnumber():
+	n = list(input('adad ha ra vared karde va ba , joda konid'))
+	print (n)
+	print (tuple(n))
+
