28e57c4acc78350872d0987131d8c3d2b13f771a
Ali Dashti <alidashti.r@gmail.com>
2018-05-29 15:08:38 +0430

    ex 5 __ 8 khordad__

--- mhmd5.0.py
+++ mhmd5.0.py
@@ -1,3 +1,4 @@
+#write a program that take 2 number and print multiple table
 input_str = input('enter 2 number split by comma : ').split(',')
 dimensions=[int(x) for x in input_str]
 rowNum=dimensions[0]
@@ -5,5 +6,10 @@ colNum=dimensions[1]
 def multi(row,col):
 	multilist =[[row*col for col in range(colNum)] for row in range(rowNum)]
 	return multilist
-
+def printtable(list):
+	for row in list:
+		for col in row:
+			print (col ,end='\t')
+		print()
 print (multi(rowNum,colNum))
+printtable(multi(rowNum,colNum))
