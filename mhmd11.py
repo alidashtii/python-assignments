684e2890c6486fee0b68a1f112e328638ee6a8ff
Ali Dashti <alidashti.r@gmail.com>
2018-05-31 15:17:44 +0430

    ex 11 --- 10 khordad --

--- mhmd11.py
+++ mhmd11.py
@@ -1,7 +1,14 @@
-number = input('enter 4 digit numbers seperate by comma :  ').split(',')
-answer=[]
-for num in number:
-	if int(num)%5 == 0:
-		answer.append(num)
 
-print (answer)
+number = [x for x in input('enter 4 digit binary numbers seperate by comma : ').split(',')]
+bnumber=[]
+for num in number:
+    x = int(num, 2)
+    if not x%5:
+        bnumber.append(num)
+print(','.join(bnumber))
+
+
+def bin_to_dec(x):
+	return int(x,2)
+def dec_to_bin(x):
+    return bin(x)[2:]
